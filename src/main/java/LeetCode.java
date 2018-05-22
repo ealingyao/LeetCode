6bfb236f31cfe0ea2d269231b643ce64787a46fa
Shengping Yao <shengpiy@usc.edu>
2018-05-21 21:11:27 -0700

    Add Test

---
+++ src/main/java/LeetCode.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ealing on 5/21/18.
+ */
+public class LeetCode {
+    public void print() {
+        System.out.println("This is LeetCode");
+    }
+}
---
+++ src/test/java/LeetCodeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ealing on 5/21/18.
+ */
+public class LeetCodeTest {
+    @Test
+    public void test() {
+        LeetCode leetCode = new LeetCode();
+        System.out.print("Test: ");
+        leetCode.print();
+    }
+}
